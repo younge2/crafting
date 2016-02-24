947d3e7713a2a06bbed3478ffbe67c35ff340416
E <young.e.f42@gmail.com>
2016-02-23 20:20:02 -0800

    Finished log in...prbly need to generalize

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var db = require('./models');
 //var request = require('request');
 var ejslayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+//var bootstrap = require('bootstrap');
+var session = require('express-session');
+var flash = require('connect-flash');
 var app=express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -10,14 +13,62 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 app.use(ejslayouts);
 
+app.use(session({
+  secret: 'dsalkfjasdflkjgdfblknbadiadsnkl',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+
 var collectionCtrl = require("./controllers/collection.js");
 app.use("/collection", collectionCtrl);
 
 var decksCtrl = require("./controllers/decks.js");
 app.use("/decks", decksCtrl);
 
+//custom middleware
+app.use(function(req, res, next) {
+  if (req.session.userId) {
+    db.user.findById(req.session.userId).then(function(user) {
+      req.currentUser = user;
+      res.locals.currentUser = user;
+      next();
+    });
+  } else {
+    req.currentUser = false;
+    res.locals.currentUser = false;
+    next();
+  }
+});
+
+
+
+
 app.get('/', function(req,res) {
+	console.log(req.session.userId);
 	res.render('index.ejs');
+});
+
+app.post('/', function(req, res) {
+  var username = req.body.userName;
+  var password = req.body.password;
+  db.user.authenticate(username, password, function(err, user) {
+    if (err) {
+      res.send(err);
+    } else if (user) {
+      req.session.userId = user.id;
+      res.redirect('/');
+    } else {
+      res.send('user and/or password invalid');
+    }
+  });
+});
+
+app.get('/logout', function(req, res) {
+  req.session.userId = false;
+  res.redirect('/');
 });
 
 app.get('/signup', function(req,res) {
@@ -25,19 +76,27 @@ app.get('/signup', function(req,res) {
 });
 
 app.post('/signup', function(req,res) {
-	db.user.findOrCreate({
-	    where: {
-	      username: req.body.potUsername
-	    },
-	    defaults: {
-	      password: req.body.potPassword
-	    }
+	if (req.body.potPassword === req.body.potPassword2){
+		db.user.findOrCreate({
+		    where: {
+		      username: req.body.potUsername
+		    },
+		    defaults: {
+		      password: req.body.potPassword
+		    }
+
     
-  }).spread(function(user, created) {
-    res.redirect('/');
-  }).catch(function(err) {
-    res.send(err);
-  });
+	  }).spread(function(user, created) {
+	  	req.session.whatever="hello!!!";
+	    res.redirect('/');
+	  }).catch(function(err) {
+	    res.send(err);
+	  });
+	} else {
+		console.log('danger');
+		req.flash('danger', 'Passwords do not match');
+      	res.render('signup.ejs', {alerts: req.flash()});
+	}
 
 });
 
--- models/user.js
+++ models/user.js
@@ -1,12 +1,46 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [8, 99]
+    }
+}
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      },
+       authenticate: function(username, password, callback) {
+          this.find({
+            where: {username: username}
+          }).then(function(user) {
+            if (!user) callback(null, false);
+            bcrypt.compare(password, user.password, function(err, result) {
+              if (err) return callback(err);
+              callback(null, result ? user : false);
+            });
+          }).catch(callback);
+        }
+    },
+
+     hooks: {
+      beforeCreate: function(user, options, callback) {
+        if (user.password) {
+          bcrypt.hash(user.password, 10, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback(null, user);
+          });
+        } else {
+          callback(null, user);
+        }
       }
     }
   });
